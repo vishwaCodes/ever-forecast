ff3d7097629d707062a43368bb9d49b794aeee4b
candytuft <vishnyc3@gmail.com>
2021-03-29 00:37:58 -0400

    Finishes up functionality to fetch and render data from the API

--- app/src/main/java/com/vishwa/weatherapp/MainActivity.kt
+++ app/src/main/java/com/vishwa/weatherapp/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.vishwa.weatherapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.AsyncTask
 import android.os.Bundle
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import org.json.JSONObject
+import java.net.URL
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -32,7 +34,7 @@ class MainActivity : AppCompatActivity() {
         override fun doInBackground(vararg p0: String?): String? {
             var response:String?
             try {
-                response = URL(spec:"https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API").readText(Charsets.UTF_8)
+                response = URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API").readText(Charsets.UTF_8)
             }
             catch (e: Exception)
             {
@@ -64,15 +66,23 @@ class MainActivity : AppCompatActivity() {
 
                 findViewById<TextView>(R.id.address).text = address
                 findViewById<TextView>(R.id.updated_at).text = updatedAtText
-                findViewById<TextView>(R.id.status).text = weatherDescription
-                findViewById<TextView>(R.id.address).text = address
-                findViewById<TextView>(R.id.address).text = address
-                findViewById<TextView>(R.id.address).text = address
-                findViewById<TextView>(R.id.address).text = address
-                findViewById<TextView>(R.id.address).text = address
-                findViewById<TextView>(R.id.address).text = address
-                findViewById<TextView>(R.id.address).text = address
+                findViewById<TextView>(R.id.status).text = weatherDescription.capitalize()
+                findViewById<TextView>(R.id.temp).text = temp
+                findViewById<TextView>(R.id.temp_min).text = tempMin
+                findViewById<TextView>(R.id.temp_max).text = tempMax
+                findViewById<TextView>(R.id.sunrise).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise*1000))
+                findViewById<TextView>(R.id.sunset).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset*1000))
+                findViewById<TextView>(R.id.wind).text = windSpeed
+                findViewById<TextView>(R.id.pressure).text = pressure
+                findViewById<TextView>(R.id.humidity).text = humidity
 
+                findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
+                findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.VISIBLE
+            }
+            catch (e: Exception)
+            {
+                findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
+                findViewById<TextView>(R.id.errorText).visibility = View.VISIBLE
             }
         }
     }
